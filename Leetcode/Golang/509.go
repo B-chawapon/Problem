9adad01180c86afa813ca12ae640471b1635a5fa
Chawapon <chawapon2001@gmail.com>
2022-12-14 16:55:21 +0700

    Create 509.go

---
+++ Leetcode/Golang/509.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	test := 7
+	fmt.Println(fib(test))
+}
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
+}
