2695cda710650f996e3f0bedfc1b5bc5fb20cd48
Chawapon <chawapon2001@gmail.com>
2023-01-03 21:59:24 +0700

    .

--- Leetcode/Golang/118.go
+++ Leetcode/Golang/118.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func main() {
+func Main() {
 	test := 7
 	fmt.Println(generate(test))
 }
---
+++ Leetcode/Golang/322.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	data2 := []int{1, 2, 5}
+	test := 10
+	fmt.Println(coinChange(data2, test))
+}
+
+func coinChange(coins []int, amount int) int {
+	if amount == 0 {
+		return 0
+	}
+	return 1
+
+}
