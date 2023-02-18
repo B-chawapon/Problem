20e8da8ccb0a6c68b92899f9a7a99ddd9f609e05
Chawapon <chawapon2001@gmail.com>
2023-02-18 23:55:22 +0700

    Update 46_Permutations.go

--- Leetcode/Golang/46_Permutations.go
+++ Leetcode/Golang/46_Permutations.go
@@ -1,3 +1,26 @@
 func permute(nums []int) [][]int {
+	answer := make([][]int, 0)
+
+	var listPermute func(startlist []int, start int)
+
+	listPermute = func(startlist []int, start int) {
+
+		if len(startlist) == len(nums) {
+			dst := make([]int, k)
+			copy(dst, startlist)
+			answer = append(answer, dst)
+			return
+		}
+
+		for i := 0; i < len(nums); i++ {
+			startlist = append(startlist, nums[i])
+			listPermute(startlist, i+1)
+			startlist = startlist[:len(startlist)-1]
+		}
+		return
+	}
+	listPermute(nums, 0)
+
+	return answer
 
 }
